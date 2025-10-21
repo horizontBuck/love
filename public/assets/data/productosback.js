74cb44325632b0e9e4ac871b0065ab6ace4b53bf
bunsen <bunsen@bunsen.bunsen>
2025-10-21 13:46:36 -0500

    textos

---
+++ public/assets/data/productosback.js
@@ -0,0 +1,464 @@
+ {
+    "id": "56",
+    "category": "pet",
+    "type": "pet",
+    "name": "Corgi Asia",
+    "gender": "dog",
+    "new": false,
+    "sale": true,
+    "rate": 5,
+    "price": 129,
+    "originPrice": 140,
+    "brand": "corgi",
+    "sold": 12,
+    "quantity": 40,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-yellow.png",
+        "image": "./assets/images/product/pets/3-2.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/3-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/3-1.png",
+      "./assets/images/product/pets/3-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/3-1.png",
+      "./assets/images/product/pets/3-2.png",
+      "./assets/images/product/pets/3-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "57",
+    "category": "pet",
+    "type": "outfit",
+    "name": "fur-lined pet coat",
+    "gender": "dog",
+    "new": false,
+    "sale": true,
+    "rate": 5,
+    "price": 129,
+    "originPrice": 140,
+    "brand": "petoutfit",
+    "sold": 24,
+    "quantity": 80,
+    "quantityPurchase": 1,
+    "sizes": [
+      "1",
+      "2",
+      "3",
+      "4"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-yellow.png",
+        "image": "./assets/images/product/pets/4-1.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/4-3.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/4-1.png",
+      "./assets/images/product/pets/4-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/4-1.png",
+      "./assets/images/product/pets/4-2.png",
+      "./assets/images/product/pets/4-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "58",
+    "category": "pet",
+    "type": "food",
+    "name": "Milo kitchen",
+    "gender": "dog",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 20,
+    "originPrice": 26,
+    "brand": "american",
+    "sold": 24,
+    "quantity": 100,
+    "quantityPurchase": 1,
+    "sizes": [
+      "200",
+      "300",
+      "500"
+    ],
+    "variation": [
+      {
+        "color": "red",
+        "colorCode": "#DB4444",
+        "colorImage": "./assets/images/product/pets/5-1.png",
+        "image": "./assets/images/product/pets/5-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/5-1.png",
+      "./assets/images/product/pets/5-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/5-1.png",
+      "./assets/images/product/pets/5-2.png",
+      "./assets/images/product/pets/5-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "Agregar al carro",
+    "slug": "pets"
+  },
+  {
+    "id": "59",
+    "category": "pet",
+    "type": "food",
+    "name": "Beneful Healthy",
+    "gender": "cat",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 16,
+    "originPrice": 22,
+    "brand": "purina",
+    "sold": 16,
+    "quantity": 80,
+    "quantityPurchase": 1,
+    "sizes": [
+      "100",
+      "200",
+      "300"
+    ],
+    "variation": [
+      {
+        "color": "red",
+        "colorCode": "#DB4444",
+        "colorImage": "./assets/images/product/pets/7-1.png",
+        "image": "./assets/images/product/pets/7-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/7-1.png",
+      "./assets/images/product/pets/7-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/7-1.png",
+      "./assets/images/product/pets/7-2.png",
+      "./assets/images/product/pets/7-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "60",
+    "category": "pet",
+    "type": "bed",
+    "name": "Bed for cat",
+    "gender": "cat",
+    "new": false,
+    "sale": true,
+    "rate": 5,
+    "price": 129,
+    "originPrice": 140,
+    "brand": "bedpet",
+    "sold": 12,
+    "quantity": 40,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3",
+      "4",
+      "5"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-white.png",
+        "image": "./assets/images/product/pets/8-3.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/8-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/8-1.png",
+      "./assets/images/product/pets/8-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/8-1.png",
+      "./assets/images/product/pets/8-2.png",
+      "./assets/images/product/pets/8-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "61",
+    "category": "pet",
+    "type": "ring",
+    "name": "Ring for cat",
+    "gender": "cat",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 16,
+    "originPrice": 22,
+    "brand": "petoutfit",
+    "sold": 24,
+    "quantity": 80,
+    "quantityPurchase": 1,
+    "sizes": [
+      "1",
+      "2",
+      "3",
+      "4"
+    ],
+    "variation": [
+      {
+        "color": "red",
+        "colorCode": "#DA4848",
+        "colorImage": "./assets/images/product/color/pet-red.png",
+        "image": "./assets/images/product/pets/6-1.png"
+      },
+      {
+        "color": "blue",
+        "colorCode": "#4856DA",
+        "colorImage": "./assets/images/product/color/pet-blue.png",
+        "image": "./assets/images/product/pets/6-2.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/6-1.png",
+      "./assets/images/product/pets/6-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/6-1.png",
+      "./assets/images/product/pets/6-2.png",
+      "./assets/images/product/pets/6-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "Agregar al carro",
+    "slug": "pets"
+  },
+  {
+    "id": "62",
+    "category": "pet",
+    "type": "bed",
+    "name": "Bed for dog",
+    "gender": "dog",
+    "new": false,
+    "sale": true,
+    "rate": 5,
+    "price": 99,
+    "originPrice": 109,
+    "brand": "bedpet",
+    "sold": 12,
+    "quantity": 40,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3",
+      "4",
+      "5"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-white.png",
+        "image": "./assets/images/product/pets/9-3.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/9-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/9-1.png",
+      "./assets/images/product/pets/9-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/9-1.png",
+      "./assets/images/product/pets/9-2.png",
+      "./assets/images/product/pets/9-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "63",
+    "category": "pet",
+    "type": "bed",
+    "name": "Bed dog",
+    "gender": "dog",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 104,
+    "originPrice": 112,
+    "brand": "bedpet",
+    "sold": 12,
+    "quantity": 40,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3",
+      "4",
+      "5"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-white.png",
+        "image": "./assets/images/product/pets/10-3.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/10-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/10-1.png",
+      "./assets/images/product/pets/10-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/10-1.png",
+      "./assets/images/product/pets/10-2.png",
+      "./assets/images/product/pets/10-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "64",
+    "category": "pet",
+    "type": "pet",
+    "name": "Cat Germany",
+    "gender": "cat",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 89,
+    "originPrice": 100,
+    "brand": "germany",
+    "sold": 16,
+    "quantity": 64,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3",
+      "4",
+      "5"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-white.png",
+        "image": "./assets/images/product/pets/8-3.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/8-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/8-1.png",
+      "./assets/images/product/pets/8-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/8-1.png",
+      "./assets/images/product/pets/8-2.png",
+      "./assets/images/product/pets/8-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
+  {
+    "id": "65",
+    "category": "pet",
+    "type": "pet",
+    "name": "dog australia",
+    "gender": "dog",
+    "new": true,
+    "sale": false,
+    "rate": 5,
+    "price": 99,
+    "originPrice": 120,
+    "brand": "australia",
+    "sold": 16,
+    "quantity": 64,
+    "quantityPurchase": 1,
+    "sizes": [
+      "2",
+      "3",
+      "4",
+      "5"
+    ],
+    "variation": [
+      {
+        "color": "yellow",
+        "colorCode": "#ECB018",
+        "colorImage": "./assets/images/product/color/pet-white.png",
+        "image": "./assets/images/product/pets/4-3.png"
+      },
+      {
+        "color": "black",
+        "colorCode": "#555",
+        "colorImage": "./assets/images/product/color/pet-black.png",
+        "image": "./assets/images/product/pets/4-1.png"
+      }
+    ],
+    "thumbImage": [
+      "./assets/images/product/pets/4-1.png",
+      "./assets/images/product/pets/4-2.png"
+    ],
+    "images": [
+      "./assets/images/product/pets/4-1.png",
+      "./assets/images/product/pets/4-2.png",
+      "./assets/images/product/pets/4-3.png"
+    ],
+    "description": "Keep your home organized, yet elegant with storage cabinets by Onita Patio Furniture. Traditionally designed, they are perfect to be used in the any place where you need to store.",
+    "action": "add to cart",
+    "slug": "pets"
+  },
